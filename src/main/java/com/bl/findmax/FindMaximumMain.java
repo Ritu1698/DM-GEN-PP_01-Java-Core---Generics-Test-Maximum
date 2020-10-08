f5a0dec00d2e830cc22c49c24efea187e2e29412
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-08 19:05:55 +0530

    [Rituparna]Refactored: Generic Class Instead Of Method

--- src/main/java/com/bl/findmax/FindMaximumMain.java
+++ src/main/java/com/bl/findmax/FindMaximumMain.java
@@ -1,8 +1,19 @@
 package com.bl.findmax;
 
-public class FindMaximumMain {
+public class FindMaximumMain<E extends Comparable<E>> {
+    E firstGenericObject;
+    E secondGenericObject;
+    E thirdGenericObject;
+    public FindMaximumMain(E firstGenericObject, E secondGenericObject, E thirdGenericObject) {
+        this.firstGenericObject = firstGenericObject;
+        this.secondGenericObject = secondGenericObject;
+        this.thirdGenericObject = thirdGenericObject;
+    }
+    public FindMaximumMain() {
+    }
+
     //Function to Find The Maximum Generic
-    public <E extends Comparable<E>> E findMaximumGeneric(E firstGeneric, E secondGeneric, E thirdGeneric) {
+    public  static <E extends Comparable<E>> E findMaximumGeneric(E firstGeneric, E secondGeneric, E thirdGeneric) {
         E maxGeneric = firstGeneric;
         if (secondGeneric.compareTo(maxGeneric) > 0) {
             maxGeneric = secondGeneric;
@@ -11,5 +22,9 @@ public class FindMaximumMain {
             maxGeneric = thirdGeneric;
         }
         return maxGeneric;
+    }
+
+    public E findMaximumGeneric() {
+        return FindMaximumMain.findMaximumGeneric(firstGenericObject, secondGenericObject, thirdGenericObject);
     }
 }
--- src/test/java/com/bl/findmax/FindMaximumTest.java
+++ src/test/java/com/bl/findmax/FindMaximumTest.java
@@ -7,74 +7,67 @@ import org.junit.Test;
 
 //Junit Testing For Different TestCases
 public class FindMaximumTest {
-    FindMaximumMain findMaximumMainObj = null;
-
-    //Initializing the Object
-    @Before
-    public void initializingObject() {
-        findMaximumMainObj = new FindMaximumMain();
-    }
 
     //Testing for Max Position At One Integer
     @Test
     public void givenThreeIntegers_whenMaxIntegerAtPositionOne_returnPositionOneInteger() {
-        Integer maximumInteger = findMaximumMainObj.findMaximumGeneric(3, 2, 1);
+        Integer maximumInteger = FindMaximumMain.findMaximumGeneric(3, 2, 1);
         assertEquals((Integer) 3, maximumInteger);
     }
 
     //Testing for Max Position At Two Integer
     @Test
     public void givenThreeIntegers_whenMaxIntegerAtPositionTwo_returnPositionTwoInteger() {
-        Integer maximumInteger = findMaximumMainObj.findMaximumGeneric(2, 3, 1);
+        Integer maximumInteger = FindMaximumMain.findMaximumGeneric(2, 3, 1);
         assertEquals((Integer) 3, maximumInteger);
     }
 
     //Testing for Max Position At Three Integer
     @Test
     public void givenThreeIntegers_whenMaxIntegerAtPositionThree_ReturnPositionThreeInteger() {
-        Integer maximumInteger = findMaximumMainObj.findMaximumGeneric(1, 2, 3);
+        Integer maximumInteger = FindMaximumMain.findMaximumGeneric(1, 2, 3);
         assertEquals((Integer) 3, maximumInteger);
     }
 
     //Testing for Max Position At One Float
     @Test
     public void givenThreeFloat_whenMaxFloatAtPositionOne_returnPositionOneFloat() {
-        Float maximumFloat = findMaximumMainObj.findMaximumGeneric(3.1f, 2.2f, 1.3f);
+        Float maximumFloat = FindMaximumMain.findMaximumGeneric(3.1f, 2.2f, 1.3f);
         assertEquals((Float) 3.1f, maximumFloat);
     }
 
     //Testing for Max Position At Two Float
     @Test
     public void givenThreeFloat_whenMaxFloatAtPositionTwo_returnPositionTwoFloat() {
-        Float maximumFloat = findMaximumMainObj.findMaximumGeneric(2.2f, 3.1f, 1.3f);
+        Float maximumFloat = FindMaximumMain.findMaximumGeneric(2.2f, 3.1f, 1.3f);
         assertEquals((Float) 3.1f, maximumFloat);
     }
 
     //Testing for Max Position At Three Float
     @Test
     public void givenThreeFloat_whenMaxFloatAtPositionThree_ReturnPositionThreeFloat() {
-        Float maximumFloat = findMaximumMainObj.findMaximumGeneric(1.3f, 2.2f, 3.1f);
+        Float maximumFloat = FindMaximumMain.findMaximumGeneric(1.3f, 2.2f, 3.1f);
         assertEquals((Float) 3.1f, maximumFloat);
     }
 
     //Testing for Max Position At One String
     @Test
     public void givenThreeString_whenMaxStringAtPositionOne_returnPositionOneString() {
-        String maximumString = findMaximumMainObj.findMaximumGeneric("Cat", "Ball", "Apple");
+        String maximumString = FindMaximumMain.findMaximumGeneric("Cat", "Ball", "Apple");
         assertEquals("Cat", maximumString);
     }
 
     //Testing for Max Position At Two String
     @Test
     public void givenThreeString_whenMaxStringAtPositionTwo_returnPositionTwoString() {
-        String maximumString = findMaximumMainObj.findMaximumGeneric("Ball", "Cat", "Apple");
+        String maximumString = FindMaximumMain.findMaximumGeneric("Ball", "Cat", "Apple");
         assertEquals("Cat", maximumString);
     }
 
     //Testing for Max Position At Three String
     @Test
     public void givenThreeString_whenMaxStringAtPositionThree_ReturnPositionThreeString() {
-        String maximumString = findMaximumMainObj.findMaximumGeneric("Ball", "Apple", "Cat");
+        String maximumString = FindMaximumMain.findMaximumGeneric("Ball", "Apple", "Cat");
         assertEquals("Cat", maximumString);
     }
 }
